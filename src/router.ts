91fdab8fc76191770a0bbd8fd9c7b7e54ad3479d
hannal <kay@hannal.net>
2019-11-20 08:01:52 +0900

    Add 스토리 상세 페이지 열기.

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Empty from './views/layouts/Empty.vue';
+import Detail from './views/layouts/Detail.vue';
 
 Vue.use(Router);
 
 export const routes = [
+  {
+    path: '/',
+    component: Detail,
+    children: [
+      {
+        path: '/stories/:code',
+        name: 'StoryDetail',
+        component: () => import('./views/pages/StoryDetail.vue'),
+      },
+    ],
+  },
   {
     path: '/',
     component: Empty,
---
+++ src/views/layouts/Detail.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .detail-layer
+    detail-header
+    router-view
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import DetailHeader from './components/DetailHeader.vue';
+
+@Component({
+  components: {
+    DetailHeader,
+  },
+})
+export default class Detail extends Vue {
+}
+</script>
